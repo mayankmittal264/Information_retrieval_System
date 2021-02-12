972a559c6abab81010bee9b2f7b63c376c382a36
Mayank Mittal <37267246+mayankmittal264@users.noreply.github.com>
2021-02-12 18:11:03 +0530

    Change views.py
    Add functionality to review text with the highest review score and helpfulness.

--- myapp/views.py
+++ myapp/views.py
@@ -7,13 +7,17 @@ from django.http import HttpResponse
 def webpage(request, *args):
     result = []
     if request.method == "POST":
+	result = []
         data = request.POST.get('data', '')
         for objects in Product.objects.all():
             if data.lower() in Product.get_text(objects).lower():
-                result.append([Product.get_score(objects), Product.get_helpfulness(objects), Product.get_text(objects)])
+                help=Product.get_helpfulness(objects)
+                good_rating=int(help.split("/")[0])
+                bad_rating=int(help.split("/")[1])-good_rating
+                result.append([Product.get_score(objects), good_rating, bad_rating, Product.get_text(objects)])
         if not result == []:
-            result.sort(key=lambda x: x[0],reverse=True)
-            output = result[0][2]
+            result.sort(key=lambda x: (-x[0], -x[1], x[2]))
+            output = result[0][3]
         else:
             output = ""
         message = {
